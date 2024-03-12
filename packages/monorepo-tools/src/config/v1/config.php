a28586b7429cc41aeaf7745394385cebf8a33c48
Alois Jaša <aloisjasa@gmail.com>
2024-03-12 10:56:17 +0100

    Odebrání nepoužívaýńých závislostí

--- packages/monorepo-tools/src/config/v1/config.php
+++ packages/monorepo-tools/src/config/v1/config.php
@@ -19,11 +19,6 @@ return static function (MBConfig $containerConfigurator): void {
 
 	$containerConfigurator->dataToAppend([
 		ComposerJsonSection::REQUIRE_DEV => [
-			'phpstan/phpstan' => '^1.7.14',
-			'phpstan/phpstan-doctrine' => '^1.3.9',
-			'phpstan/phpstan-nette' => '^1.0.0',
-			'phpstan/phpstan-phpunit' => '^1.1',
-			'phpunit/phpunit' => '^9.5 || ^10',
 			'symplify/monorepo-builder' => '11.1.17',
 		],
 	]);
